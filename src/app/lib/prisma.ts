bbb02ee5b4bd3100af579a710218d0e43cfab4d1
t4415 <t44150703@yahoo.co.jp>
2025-05-28 22:01:11 +0900

    pathの設定の変更

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,14 +1,11 @@
-import { PrismaClient } from "../..//generated/prisma";
-let prisma: PrismaClient;
-const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
+/* eslint-disable prefer-const */
+import { PrismaClient } from "@/generated/prisma"
+let prisma: PrismaClient
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined
+}
 if (!globalForPrisma.prisma) {
-    try {
-        globalForPrisma.prisma = new PrismaClient();
-    } catch (e) {
-        throw new Error(
-            "@prisma/client did not initialize yet. Please run 'npx prisma generate' and restart the server."
-        );
-    }
+    globalForPrisma.prisma = new PrismaClient();
 }
 prisma = globalForPrisma.prisma;
-export default prisma;
+export default prisma
