b3161a31aa3013244a2aa30811a6dfb133059fb2
t4415 <t44150703@yahoo.co.jp>
2025-05-28 22:27:24 +0900

    デプロイ時の修正

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../..//generated/prisma";
+import { PrismaClient } from "../../generated/prisma";
 let prisma: PrismaClient;
 const globalForPrisma = global as unknown as { prisma: PrismaClient | undefined };
 if (!globalForPrisma.prisma) {
