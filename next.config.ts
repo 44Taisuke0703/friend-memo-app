fb240d26b24d2c0f55a47bba7f794bea5a935505
t4415 <t44150703@yahoo.co.jp>
2025-05-28 22:05:16 +0900

    next.configの設定の変更

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.resolve.alias['@/generated/prisma'] = path.resolve(__dirname, 'src/generated/prisma');
+    return config;
+  },
 };
 
 export default nextConfig;
